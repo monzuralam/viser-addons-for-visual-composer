97b15388a2494ce7d9c153fbd033ed2ed2295a6b
monzuralam <monzur707@gmail.com>
2021-06-08 21:21:03 +0600

    plugin info added

---
+++ viser-addons-for-visual-composer.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: VISER Addons for WPBakery Page Builder
+Plugin URI: http://viserx.com
+Description: VISER Addons is a powerfull widgets that works seamlessly with WPBakery Page Builder. It's extremely powerful for any theme.
+Version: 0.0.1
+Author: Monzur Alam
+Author URI: https://profiles.wordpress.org/monzuralam
+text-domain: viser-addons-for-visual-composer
+License: GPLv2 or later
+*/
